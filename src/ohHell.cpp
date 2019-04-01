dd2747b6b2e293eb526815e5c1e58e98d2ad2090
LeBrendanJames <35370736+LeBrendanJames@users.noreply.github.com>
2019-04-01 06:56:53 -0500

    Move ohHell.cpp into src folder

---
+++ src/ohHell.cpp
@@ -0,0 +1,28 @@
+// Program to find optimal bid and play given game parameters  
+// https://en.wikipedia.org/wiki/Oh_Hell
+
+#include <iostream>
+#include <vector>
+#include <random>
+//#include "Game.h"
+#include "GameState.h"
+#include "DecisionPoint.h"
+
+
+
+int main(){
+	// Get user input on current game
+		// How many players?
+		// Hero position?
+		// Number of cards in round?
+		// Any bids yet?
+		// Any full tricks played yet?
+		// Any cards played yet?
+	
+	// Create Game & GameState 
+	
+	game->simRound();
+	
+	
+	return 0;
+}
